fb08444bd1878ab46d3174e8e8dcbf10370008f8
garrethil <garrethildebrandt@gmail.com>
2024-02-21 21:13:29 -0500

    correct foreign key references

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,25 @@ CREATE DATABASE employeeData_db;
 USE employeeData_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-  ON DELETE SET NULL
+  id INT NOT NULL PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
-  REFERENCES department(id),
-  PRIMARY KEY (id)
-  ON DELETE SET NULL
+  REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  id INT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES roles(id)
-  
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id),
+  CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
