1d8cce35c4961dc7236cfe0ff3ba847238c43ed5
garrethil <garrethildebrandt@gmail.com>
2024-02-23 21:48:59 -0500

    add then block to inquirer statement

--- index.js
+++ index.js
@@ -19,11 +19,27 @@ const db = mysql.createConnection(
 inquirer.prompt([
     {
         type: "list",
-        name: "actions",
+        name: "action",
         message: "Please select a license for your project.",
         choices: ["View All Employees", "Add Employee", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department"],
       },
-])
+]).then((answers) => {
+  if (answers.action === "View All Employees") {
+
+  } else if (answers.action === "Add Employee") {
+
+  }else if (answers.action === "Update Employee Role") {
+ 
+  } else if (answers.action === "View all Roles") {
+
+  } else if (answers.action === "Add Role") {
+    
+  } else if (answers.action === "View All Departments") {
+    
+  } else if (answers.action === "Add Department") {
+    
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
