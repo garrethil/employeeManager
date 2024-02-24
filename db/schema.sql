35b17dedee314da1d6def720ebf5b3d0c3e0cf7c
garrethil <garrethildebrandt@gmail.com>
2024-02-24 13:30:25 -0500

    add prompt to insert new department to department table

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employeeData_db;
 USE employeeData_db;
 
 CREATE TABLE department (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE roles (
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,18 +16,27 @@ const db = mysql.createConnection(
   console.log(`Connected to the employeeData_db database.`)
 );
 
+db.connect();
 
-inquirer.prompt([
+function startPrompt() {
+  inquirer.prompt([
     {
         type: "list",
         name: "action",
-        message: "Please select a license for your project.",
+        message: "What would you like to do?",
         choices: ["View All Employees", "Add Employee", "Update Employee Role", "View all Roles", "Add Role", "View All Departments", "Add Department"],
       },
 ]).then((answers) => {
   if (answers.action === "View All Employees") {
-    db.query('SELECT emp.first_name, emp.last_name, r.title, r.salary, d.department_name, emp2.first_name as manager_first_name, emp2.last_name as manager_last_name FROM employeeData_db.employee as emp LEFT JOIN employeeData_db.roles as r ON emp.role_id = r.id LEFT JOIN employeeData_db.department as d ON r.department_id = d.id LEFT JOIN employeeData_db.employee as emp2 ON emp.manager_id = emp2.id', function (err, results) {
+    const empTabQ = `SELECT emp.first_name, emp.last_name, r.title as role, r.salary, d.department_name, emp2.first_name as manager_first_name, emp2.last_name as manager_last_name 
+                     FROM employeeData_db.employee as emp LEFT JOIN employeeData_db.roles as r ON emp.role_id = r.id 
+                     LEFT JOIN employeeData_db.department as d ON r.department_id = d.id 
+                     LEFT JOIN employeeData_db.employee as emp2 ON emp.manager_id = emp2.id`;
+
+    db.query(empTabQ, function (err, results) {
       console.table(results);
+
+      startPrompt();
     });
 
   } else if (answers.action === "Add Employee") {
@@ -34,21 +44,68 @@ inquirer.prompt([
   }else if (answers.action === "Update Employee Role") {
  
   } else if (answers.action === "View all Roles") {
+const empRolQ = `SELECT r.title, r.salary, d.department_name as department 
+                 FROM employeeData_db.roles as r 
+                 LEFT JOIN employeeData_db.department as d ON r.department_id = d.id`;
 
-
-    db.query('SELECT r.title, r.salary, d.department_name as department FROM employeeData_db.roles as r LEFT JOIN employeeData_db.department as d ON r.department_id = d.id', function (err, results) {
+    db.query(empRolQ, function (err, results) {
       console.table(results);
+      startPrompt();
     });
   } else if (answers.action === "Add Role") {
+    db.query('SELECT department_name FROM department', function (error, results) {
+const departments = results.map(row => row.department_name);
+
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "newRole",
+          message: "What is the name of the role you would like to add?",
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the salary for the new role?",
+        },
+        {
+          type: "list",
+          name: "roleDept",
+          message: "What department does the new role belong to?",
+          choices: departments
+        },
+      ])
+    })
     
   } else if (answers.action === "View All Departments") {
     db.query('SELECT id, department_name as department FROM department', function (err, results) {
       console.table(results);
+      startPrompt();
     });
   } else if (answers.action === "Add Department") {
-    
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "newDepartment",
+        message: "What is the name of the department you would like to add?",
+      },
+    ]).then((answers) => {
+      const dept = answers.newDepartment;
+      db.query(`INSERT INTO department (department_name) VALUES ('${dept}')`, function (err, results) {
+
+        if (err) {
+          console.log(err);
+        }
+        console.log(`${dept} department added to the department table`);
+        startPrompt();
+      });
+    })
   }
 })
+}
+
+startPrompt();
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
