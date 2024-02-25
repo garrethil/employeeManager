82aa950dda0322d307ff4df00dc3789b88b9ac9f
garrethil <garrethildebrandt@gmail.com>
2024-02-25 18:12:28 -0500

    handle update eployee role option

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function startPrompt() {
       },
 ]).then((answers) => {
   if (answers.action === "View All Employees") {
-    const empTabQ = `SELECT emp.first_name, emp.last_name, r.title as role, r.salary, d.department_name, emp2.first_name as manager_first_name, emp2.last_name as manager_last_name 
+    const empTabQ = `SELECT emp.first_name, emp.last_name, r.title as role, r.salary, d.department_name as department, emp2.first_name as manager_first_name, emp2.last_name as manager_last_name 
                      FROM employeeData_db.employee as emp LEFT JOIN employeeData_db.roles as r ON emp.role_id = r.id 
                      LEFT JOIN employeeData_db.department as d ON r.department_id = d.id 
                      LEFT JOIN employeeData_db.employee as emp2 ON emp.manager_id = emp2.id`;
@@ -130,7 +130,46 @@ function startPrompt() {
       })}
   })});
   }else if (answers.action === "Update Employee Role") {
- 
+    db.query('SELECT first_name, last_name FROM employee', function (error, results) {
+      const employeeOpt = results.map(row => `${row.first_name} ${row.last_name}`);
+      db.query('SELECT title FROM roles', function (error, results) {
+        const roles = results.map(row => row.title);
+      
+    
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "empUpdate",
+        message: "Which employee's role do you want to update?",
+        choices: employeeOpt
+      },
+      {
+        type: "list",
+        name: "updatedRole",
+        message: "Which role do you want to assign to the employee?",
+        choices: roles
+      },
+    ]).then((answers) => {
+      let nameArray = answers.empUpdate.split(" ");
+      db.query(`SELECT id FROM roles WHERE title = '${answers.updatedRole}'`, function (error, results) {
+        if (error) {
+          console.log(error)
+        } 
+        let roleId = results[0].id;
+        db.query(`UPDATE employee SET role_id = ${roleId} WHERE first_name = '${nameArray[0]}' AND last_name = '${nameArray[1]}'`, function (error, results) {
+          if (error) {
+            console.log(error);
+          } 
+            console.log('Employees Role updated');
+            startPrompt();
+        }
+      )}
+    )})
+      
+
+      
+    
+  }) })
   } else if (answers.action === "View all Roles") {
 const empRolQ = `SELECT r.title, r.salary, d.department_name as department 
                  FROM employeeData_db.roles as r 
@@ -141,7 +180,7 @@ const empRolQ = `SELECT r.title, r.salary, d.department_name as department
       startPrompt();
     });
   } else if (answers.action === "Add Role") {
-    db.query('SELECT department_name FROM department', function (error, results) {
+    db.query('SELECT department_name FROM department', function (error, results)  {
 const departments = results.map(row => row.department_name);
 
       inquirer.prompt([
